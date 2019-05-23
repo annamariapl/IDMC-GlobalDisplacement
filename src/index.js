88eb1451b2a4862e7057ada06e00ff2ca5634d4a
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2019-05-23 11:34:41 +0200

    refactoring + .gitignore

--- src/index.js
+++ src/index.js
@@ -10,56 +10,59 @@ class App extends React.Component {
     data: []
   };
 
-
   componentDidMount() {
-    function uniqueArray(myArray, myFunction) {
-      return Array.from(new Set(myArray.map(el => myFunction(el)))).sort((a, b) => a - b);
-    }
+        //  without uniqueArray: const years = Array.from(new Set(data.results.map(el => el.year))).sort((a, b) => a - b);
+       // function uniqueArray returns a sorted Array without duplicates. It requires an Array that you want to sort, and the information what you want to map(el)
+       const uniqueArray = (myArray, myFunction) => {
+        return [...new Set(myArray.map(el => myFunction(el)))].sort((a, b) => a - b);
+      }
 
-    function getHazardCategory(value) {
+      const getHazardCategory = (value) => {
+      // does two things: code flexible for API changes (if there will be new hazard_categories) && prevents "undefinced" hussle
       return value === undefined ? "other" : value;
     }
 
     axios
-      .get("https://api.idmcdb.org/api/disaster_data?ci=IDMCWSHSOLO009")
-      .then(({ data }) => {
-      //  const years = Array.from(new Set(data.results.map(el => el.year))).sort((a, b) => a - b);
-        const years = uniqueArray(data.results, el => el.year);
-        const hazard_cats = uniqueArray(data.results, el => el.hazard_category);
-        hazard_cats_names = uniqueArray(data.results, el => getHazardCategory(el.hazard_category));
+    .get("https://api.idmcdb.org/api/disaster_data?ci=IDMCWSHSOLO009")
+    .then(({ data }) => {
+      const years = uniqueArray(data.results, el => el.year);
+      const hazard_cats = uniqueArray(data.results, el => el.hazard_category);
+      hazard_cats_names = uniqueArray(data.results, el => getHazardCategory(el.hazard_category));
 
-        const graphData = years.map(item => {
-          let result = {};
-          result["year"] = item;
+      const graphData = years.map(item => {
+        const result = {};
+        result["year"] = item;
 
-          for (let [key, value] of Object.entries(hazard_cats)) {
-            let hazCatVal = data.results
-              .filter(el => el.hazard_category === value)
-              .filter(i => i.year === item)
-              .reduce((accum, item) => accum + item.new_displacements || 0, 0);
-            result[getHazardCategory(value)] = hazCatVal;
-          }
-          return result;
-        });
-
-        console.log("graph data", graphData);
-
-
-        this.setState({
-          data: graphData
-        });
-      })
-      .catch(error => {
-        console.log(error);
+        for (const value of Object.values(hazard_cats)) {
+          const hazardCategoryValues = data.results
+          .filter(e => e.hazard_category === value)
+          .filter(e=> e.year === item)
+          .reduce((displacement_accumulator, element) => displacement_accumulator + element.new_displacements || 0, 0);
+          result[getHazardCategory(value)] = hazardCategoryValues;
+        }
+        return result;
       });
+
+// final check what goes to the graph
+// console.log("graph data", graphData);
+
+
+this.setState({
+  data: graphData
+});
+})
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Bar data={this.state.data} />
+      <h3> Global (worldwide) displacements by different causes </h3>
+      <Bar data={this.state.data} />
       </div>
-    );
+      );
   }
 }
 
