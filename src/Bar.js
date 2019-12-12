6b94d66bbd4050c177ccffbc8117a8e66ce5aa67
annamaria <annamaria.wojtyga@gmail.com>
2019-12-12 23:03:59 +0100

    minor changes to improve estetics

--- src/Bar.js
+++ src/Bar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
 import PropTypes from "prop-types";
-import {hazard_cats_names} from "./index.js";
+import { hazard_cats_names } from "./index.js";
 
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
@@ -9,100 +9,99 @@ import {hazard_cats_names} from "./index.js";
 // website examples showcase many properties,
 // you'll often use just a few of them.
 export const Bar = props => (
-  
   <div style={{ height: "500px", width: "500px" }}>
-  <ResponsiveBar
-  data={props.data}
-  keys={hazard_cats_names}
-  indexBy="year"
-  margin={{
-    top: 50,
-    right: 130,
-    bottom: 50,
-    left: 60
-  }}
-  padding={0.3}
-  colors={{
-    scheme: "nivo"
-  }}
-  defs={[
-    {
-      id: "dots",
-      type: "patternDots",
-      background: "inherit",
-      color: "#38bcb2",
-      size: 4,
-      padding: 1,
-      stagger: true
-    },
-    {
-      id: "lines",
-      type: "patternLines",
-      background: "inherit",
-      color: "#eed312",
-      rotation: -45,
-      lineWidth: 6,
-      spacing: 10
-    }
-    ]}
-    borderColor={{
-      from: "color",
-      modifiers: [["darker", 1.6]]
-    }}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "year",
-      legendPosition: "middle",
-      legendOffset: 32
-    }}
-    axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "displacements",
-      legendPosition: "middle",
-      legendOffset: -40
-    }}
-    labelSkipWidth={0}
-    labelSkipHeight={0}
-    labelTextColor={{
-      from: "color",
-      modifiers: [["darker", 1.6]]
-    }}
-    legends={[
-      {
-        dataFrom: "keys",
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: "left-to-right",
-        itemOpacity: 0.85,
-        symbolSize: 20,
-        effects: [
+    <ResponsiveBar
+      data={props.data}
+      keys={hazard_cats_names}
+      indexBy="year"
+      layout="horizontal"
+      margin={{
+        top: 50,
+        right: 130,
+        bottom: 50,
+        left: 60
+      }}
+      padding={0.3}
+      colors={{
+        scheme: "nivo"
+      }}
+      defs={[
         {
-          on: "hover",
-          style: {
-            itemOpacity: 1
-          }
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "#38bcb2",
+          size: 4,
+          padding: 1,
+          stagger: true
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "#eed312",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10
         }
-        ]
-      }
+      ]}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]]
+      }}
+      axisTop={{ legend: "displacements", tickPadding: 10000, tickSize: 0 }}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 3,
+        tickPadding: 10,
+        tickRotation: 45,
+        legendPosition: "end",
+        legendOffset: 5
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "year",
+        legendPosition: "middle",
+        legendOffset: -40
+      }}
+      labelSkipWidth={0}
+      labelSkipHeight={0}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]]
+      }}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1
+              }
+            }
+          ]
+        }
       ]}
       animate={true}
       motionStiffness={90}
       motionDamping={15}
-      />
-      </div>
-      );
+    />
+  </div>
+);
 
 // Bar expects following format to work
 Bar.propTypes = {
@@ -110,7 +109,7 @@ Bar.propTypes = {
     PropTypes.shape({
       year: PropTypes.number.isRequired,
       Geophysical: PropTypes.number.isRequired,
-      "Weather related": PropTypes.number.isRequired,
+      "Weather related": PropTypes.number.isRequired
     })
-    ).isRequired
+  ).isRequired
 };
